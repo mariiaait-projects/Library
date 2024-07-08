496e68d751fa138b7230afa7ad19963b5a6b7c9e
AndersenGAV <Andersengal@gmail.com>
2024-07-08 18:44:40 +0300

    last 08.07

--- sitelibrary/library/queries.py
+++ sitelibrary/library/queries.py
@@ -8,3 +8,16 @@ from library.models import Book
 
 print(Book.objects.all().order_by('published_year'))
 print(Book.objects.all())
+#
+# print(Book.objects.filter(genre = "Romance"))
+#
+# print(Book.objects.filter(published_year__gt=1950))
+#
+# print(Book.objects.filter(author = "Harper Lee"))
+#
+# print(Book.objects.get(ISBN = "9780345339683"))
+
+# book = Book.objects.get(ISBN="9780345339683")
+# book.title = "ROBOCOP"
+# book.save()
+print(Book.objects.filter(ISBN="9780345339683").update(title="ROBOCOP"))
