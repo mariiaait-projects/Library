95aff00a86bef206861ff540b07350df7c76a921
sergbash1991 <sergbash1991@gmail.com>
2024-07-10 18:59:15 +0300

    Added random and datetime

--- sitelibrary/library/queries.py
+++ sitelibrary/library/queries.py
@@ -1,13 +1,14 @@
 import os
 import django
-
+from django.template import Library
+import random
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitelibrary.settings')
 django.setup()
 
 from library.models import Book
 
-print(Book.objects.all().order_by('published_year'))
-print(Book.objects.all())
+# print(Book.objects.all().order_by('published_year'))
+# print(Book.objects.all())
 #
 # print(Book.objects.filter(genre = "Romance"))
 #
@@ -20,11 +21,24 @@ print(Book.objects.all())
 # book = Book.objects.get(ISBN="9780345339683")
 # book.title = "ROBOCOP"
 # book.save()
-print(Book.objects.filter(ISBN="9780345339683").update(title="ROBOCOP"))
+# print(Book.objects.filter(ISBN="9780345339683").update(title="ROBOCOP"))
+#
+# print(Book.objects.filter(author='Charlotte Bronte').update(genre='Mystery'))
 
-print(Book.objects.filter(author='Charlotte Bronte').update(genre='Mystery'))
+# books = Book.objects.filter(published_year__lt=2000)
+# for book in books:
+#     book.genre = 'Классическая литература: ' + book.genre
+#     book.save()
 
-books = Book.objects.filter(published_year__lt=2000)
-for book in books:
-    book.genre = 'Классическая литература: ' + book.genre
-    book.save()
+# ids = Book.objects.values_list('id', flat=True)
+# print(ids)
+# random_ids = random.sample(list(ids), 3)
+# print(random_ids)
+# for id in random_ids:
+#     Book.objects.filter(id=id).update(genre='')
+
+# Book.objects.filter(genre='').update(ISBN='UNKNOWN')
+
+# import datetime
+# Book.objects.filter(title__icontains='great').update(published_year=datetime.datetime.now().year)
+
