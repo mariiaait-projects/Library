f70397e50d57be7fa759651316b82d5676d2991d
AndersenGAV <Andersengal@gmail.com>
2024-08-19 19:05:47 +0300

    Select 19/08

--- sitelibrary/library/forms.py
+++ sitelibrary/library/forms.py
@@ -7,11 +7,12 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'authors', 'published_year', 'genre', 'ISBN']
+        authors = forms.ModelMultipleChoiceField(queryset=Author.objects.all(), widget=forms.CheckboxSelectMultiple,
+                                                required=True)
+        genre = forms.ModelChoiceField(queryset=Genre.objects.all(), widget=forms.Select, required=True)
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Enter book name'}),
-            'authors': forms.MultipleChoiceField(queryset=Author.objects.all()),
+            'title': forms.TextInput(attrs={'placeholder': 'Enter book name', "class": "form-control"}),
             'published_year': forms.NumberInput(attrs={'placeholder': 'Enter book\'s year'}),
-            'genre': forms.Select(attrs={}),
             'ISBN': forms.TextInput(attrs={'placeholder': 'Enter ISBN number'})
         }
 
