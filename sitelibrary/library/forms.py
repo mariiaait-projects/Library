c7414a282fa87cf1fa10739dcce5943b986ad229
shana <bashkatovahelga@gmail.com>
2024-08-19 17:37:07 +0200

    Initial

--- sitelibrary/library/forms.py
+++ sitelibrary/library/forms.py
@@ -7,6 +7,13 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'authors', 'published_year', 'genre', 'ISBN']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter book name'}),
+            'authors': forms.MultipleChoiceField(queryset=Author.objects.all()),
+            'published_year': forms.NumberInput(attrs={'placeholder': 'Enter book\'s year'}),
+            'genre': forms.Select(attrs={}),
+            'ISBN': forms.TextInput(attrs={'placeholder': 'Enter ISBN number'})
+        }
 
 class GenreForm(forms.ModelForm):
     class Meta:
