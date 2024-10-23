e3d8d2105314b9d8cd4bcc142b72b918c3a67ab4
AndersenGAV <Andersengal@gmail.com>
2024-10-23 19:31:51 +0300

    JS 23/10

--- sitelibrary/library/static/library/js/cart_update_quantity.js
+++ sitelibrary/library/static/library/js/cart_update_quantity.js
@@ -1,5 +1,9 @@
-console.log("Hello");
 document.addEventListener('DOMContentLoader', function() {
-    const inputs = document.querySelectorAll("product-quantity");
-    console.log("Hello");
+    const inputs = document.querySelectorAll(".product-quantity");
+
+    inputs.forEach(function(input){
+        input.addEventListener('input', function(event){
+            console.log("Hello");
+        });
+    });
 });
