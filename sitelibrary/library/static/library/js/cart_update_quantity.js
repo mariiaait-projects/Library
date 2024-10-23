555fe3e17bd08851a7e19d185a58e032f02820a6
shana <bashkatovahelga@gmail.com>
2024-10-23 18:01:43 +0200

    Initial

--- sitelibrary/library/static/library/js/cart_update_quantity.js
+++ sitelibrary/library/static/library/js/cart_update_quantity.js
@@ -1,1 +1,5 @@
-console.log('Hello');
+console.log("Hello");
+document.addEventListener('DOMContentLoader', function() {
+    const inputs = document.querySelectorAll("product-quantity");
+    console.log("Hello");
+});
