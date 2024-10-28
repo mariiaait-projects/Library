108fd760ede3c4015e555510cab1638ad95c4cc7
sergbash1991 <sergbash1991@gmail.com>
2024-10-28 19:04:45 +0200

    Changed margin-top for h2

--- sitelibrary/library/static/library/js/cart_update_quantity.js
+++ sitelibrary/library/static/library/js/cart_update_quantity.js
@@ -5,8 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
             let purchase_id = event.target.getAttribute('data-purchase-id');
             let quantity = event.target.value;
             let price = event.target.getAttribute('data-price');
-            let unit_price_element = document.querySelector('#unit-price-${purchase_id}')
-            console.log(unit_price_element);
+            let unit_price_element = document.querySelector(`#unit-price-${purchase_id}`);
+            let url = event.target.getAttribute('data-update-url');
+            let total_price_element =  document.querySelector('#total-price');
+
+            unit_price_element.textContent = (price * quantity).toFixed(2);
+            total_price_element.textContent = 0;
+
+            updateDatabase(url, purchase_id, quantity);
         });
     });
 });
