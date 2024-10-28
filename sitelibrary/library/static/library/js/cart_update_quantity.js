09433b367aeaecf7cd22186551c68f3e8f143d71
AndersenGAV <andersengal@gmail.com>
2024-10-28 18:50:32 +0200

    price 28/10

--- sitelibrary/library/static/library/js/cart_update_quantity.js
+++ sitelibrary/library/static/library/js/cart_update_quantity.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputs = document.querySelectorAll(".product-quantity")
     inputs.forEach(function(input){
         input.addEventListener('input', function(event){
-
+            let purchase_id = event.target.getAttribute('data-purchase-id');
+            let quantity = event.target.value;
+            let price = event.target.getAttribute('data-price');
+            let unit_price_element = document.querySelector('#unit-price-${purchase_id}')
+            console.log(unit_price_element);
         });
     });
 });
